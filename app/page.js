06d0ff29bd31af4a35500eaae9046ed2b5496c5e
farrukh-Zain <121316072+farrukh-Zain@users.noreply.github.com>
2024-03-13 23:55:01 +0500

    docs_mini-cards

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Background from '@/docs/src/Components/Background'
+import Forground from '@/docs/src/Components/Forground'
+
+
+const page = () => {
+  
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+     
+     
+      <Background />
+    
+      <Forground />
+    </div>
+  )
+}
+
+export default page
+
